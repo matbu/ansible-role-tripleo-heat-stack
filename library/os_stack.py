2cbf5c7f88a9067056906e15dcf87ab5d87692bc
Mathieu Bultel <mbultel@redhat.com>
2016-04-04 23:49:03 +0200

    Update heat module for adding more functionalities
    * Add list, update and debug state to heat ansible module
    * Remove the used of Shade since, Shade doesnt have
      all heat binding : resource-xx deployment-xx

--- library/os_stack.py
+++ library/os_stack.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python
 #coding: utf-8 -*-
 
-from time import sleep
 try:
-    import shade
-    HAS_SHADE = True
+    import time
+    from keystoneclient.v2_0 import client as ksclient
+    from heatclient.client import Client
+    from heatclient.common import template_utils
+    from heatclient.common import utils
 except ImportError:
-    HAS_SHADE = False
+    print("failed=True msg='heatclient and keystoneclient is required'")
 
 DOCUMENTATION = '''
 ---
 module: os_stack
-short_description: Add/Remove Heat Stack
-extends_documentation_fragment: openstack
-version_added: "2.0"
-author: "Mathieu Bultel (matbu)"
-description:
-   - Add or Remove a Stack to an OpenStack Heat
+   - Create, update, list, delete and debug failure on heat stack deployment
 options:
+   login_username:
+     description:
+        - login username to authenticate to keystone
+     required: true
+     default: admin
+   login_password:
+     description:
+        - Password of login user
+     required: true
+     default: True
+   login_tenant_name:
+     description:
+        - The tenant name of the login user
+     required: true
+     default: True
+   auth_url:
+     description:
+        - The keystone URL for authentication
+     required: false
+     default: 'http://127.0.0.1:35357/v2.0/'
+   region_name:
+     description:
+        - Name of the region
+     required: false
+     default: None
    state:
      description:
         - Indicate desired state of the resource
-     choices: ['present', 'absent']
-     required: false
+     choices: ['create', 'update', 'list', 'delete', 'debug']
      default: present
-   name:
+   stack_name:
      description:
         - Name of the stack that should be created
      required: true
@@ -34,105 +55,163 @@ options:
        - Path of the template file to use for the stack creation
      required: false
      default: None
-   environment:
+   environment_files:
      description:
         - List of environment files that should be used for the stack creation
      required: false
      default: None
-   timeout:
-     description:
-        - Maximum number of seconds to wait for the stack creation
-     required: false
-     default: 180
-requirements:
-    - "python >= 2.6"
-    - "shade"
+requirements: ["heatclient", "keystoneclient"]
 '''
 
 EXAMPLES = '''
 # Create a stack with given template and environment files
+    - name: create stack
+      heat_stack:
+        login_username: admin
+        login_password: admin
+        auth_url: "http://192.168.1.14:5000/v2.0"
+        tenant_name: admin
+        stack_name: test
+        state: create
+        template: "/home/stack/ovb/templates/quintupleo.yaml"
+        environment_files: ['/home/stack/ovb/templates/resource-registry.yaml','/home/stack/ovb/templates/env.yaml']
+
+    - name: delete stack
+      heat_stack:
+        stack_name: test
+        state: delete
+        login_username: admin
+        login_password: admin
+        auth_url: "http://192.168.1.14:5000/v2.0"
+        tenant_name: admin
 '''
 
-def _create_stack(module, stack, cloud):
-    try:
-        stack = cloud.create_stack(module.params['name'],
-                                   template_file=module.params['template'],
-                                   files=module.params['environment_files'],
-                                   timeout=module.params['timeout'],
-                                   wait=True)
+def obj_gen_to_dict(gen):
+    """Enumerate through generator of object and return lists of dictonaries.
+    """
+    obj_list = []
+    for obj in gen:
+        obj_list.append(obj.to_dict())
+    return obj_list
 
-        stack = cloud.get_stack(stack['stack']['id'], None)
+
+class Stack(object):
+
+    def __init__(self, kwargs):
+        self.client = self._get_client(kwargs)
+
+    def _get_client(self, kwargs, endpoint_type='publicURL'):
+        """ get heat client """
+        kclient = ksclient.Client(**kwargs)
+        token = kclient.auth_token
+        endpoint = kclient.service_catalog.url_for(service_type='orchestration',
+                                                    endpoint_type=endpoint_type)
+        kwargs = {
+                'token': token,
+        }
+        return Client('1', endpoint=endpoint, token=token)
+
+    def create(self, name,
+                template_file,
+                env_file=None,
+                format='json'):
+        """ create heat stack with the given template and environment files """
+        self.client.format = format
+        tpl_files, template = template_utils.get_template_contents(template_file)
+        env_files, env = template_utils.process_multiple_environments_and_files(env_paths=env_file)
+
+        stack = self.client.stacks.create(stack_name=name,
+                                   template=template,
+                                   environment=env,
+                                   files=dict(list(tpl_files.items()) + list(env_files.items())),
+                                   parameters={})
+        uid = stack['stack']['id']
+
+        stack = self.client.stacks.get(stack_id=uid).to_dict()
+        while stack['stack_status'] == 'CREATE_IN_PROGRESS':
+            stack = self.client.stacks.get(stack_id=uid).to_dict()
+            time.sleep(5)
         if stack['stack_status'] == 'CREATE_COMPLETE':
             return stack
         else:
-            module.fail_json(msg = "Failure in creating stack: ".format(stack))
-    except shade.OpenStackCloudException as e:
-        module.fail_json(msg=e.message)
+            return False
 
-def _system_state_change(module, stack, cloud):
-    state = module.params['state']
-    if state == 'present':
-        if not stack:
-            return True
-    if state == 'absent' and stack:
-        return True
-    return False
+    def list(self):
+        """ list created stacks """
+        fields = ['id', 'stack_name', 'stack_status', 'creation_time',
+                  'updated_time']
+        uids = []
+        stacks = self.client.stacks.list()
+        utils.print_list(stacks, fields)
+        return obj_gen_to_dict(stacks)
+
+    def delete(self, name):
+        """ delete stack with the given name """
+        self.client.stacks.delete(name)
+        return self.list()
+
+    def get_id(self, name):
+        """ get stack id by name """
+        stacks = self.client.stacks.list()
+        while True:
+            try:
+                stack = stacks.next()
+                if name == stack.stack_name:
+                    return stack.id
+            except StopIteration:
+                break
+                return False
 
 def main():
 
-    argument_spec = openstack_full_argument_spec(
-        name=dict(required=True),
-        template=dict(default=None),
-        environment_files=dict(default=None, type='dict'),
-        timeout=dict(default=180),
-        state=dict(default='present', choices=['absent', 'present']),
-    )
 
-    module_kwargs = openstack_module_kwargs()
-    module = AnsibleModule(argument_spec,
-                           supports_check_mode=True,
-                           **module_kwargs)
-
-    if not HAS_SHADE:
-        module.fail_json(msg='shade is required for this module')
-
+    argument_spec = openstack_argument_spec()
+    argument_spec.update(dict(
+            stack_name              = dict(required=True),
+            template                = dict(default=None),
+            environment_files       = dict(default=None, type='list'),
+            state                   = dict(default='create', choices=['create', 'update', 'delete', 'list', 'debug']),
+            tenant_name             = dict(default=None),
+            timeout                 = dict(default=180),
+    ))
+    module = AnsibleModule(argument_spec=argument_spec)
     state = module.params['state']
-    name = module.params['name']
+    stack_name = module.params['stack_name']
     template = module.params['template']
     environment_files = module.params['environment_files']
+    kwargs = {
+                'username':  module.params['login_username'],
+                'password':  module.params['login_password'],
+                'tenant_name':  module.params['tenant_name'],
+                'auth_url':  module.params['auth_url']
+            }
 
-    # Check for required parameters when state == 'present'
-    if state == 'present':
-        for p in ['template']:
-            if not module.params[p]:
-                module.fail_json(msg='%s required with present state' % p)
+    stack = Stack(kwargs)
 
-    try:
-        cloud = shade.openstack_cloud(**module.params)
-        stack = cloud.get_stack(name)
-
-        if state == 'present':
+    if module.params['state'] == 'create':
+        stack_id = stack.get_id(stack_name)
+        if not stack_id:
+            stack = stack.create(name=stack_name,
+                                        template_file=template,
+                                        env_file=environment_files)
             if not stack:
-                stack = _create_stack(module, stack, cloud)
-                changed = True
-            else:
-                changed = False
-            module.exit_json(changed=changed,
-                             stack=stack,
-                             id=stack['id'])
-        elif state == 'absent':
-            if not stack:
-                changed = False
-            else:
-                changed = True
-                if not cloud.delete_stack(name):
-                    module.fail_json(msg='delete stack failed for stack: %s' % name)
-            module.exit_json(changed=changed)
+                module.fail_json(msg="Failed to create stack")
+            module.exit_json(changed = True, result = "created" , stack = stack)
+        else:
+            module.exit_json(changed = False, result = "success" , id = stack_id)
+    elif module.params['state'] == 'delete':
+        stack_id = stack.get_id(stack_name)
+        if not stack_id:
+            module.exit_json(changed = False, result = "success")
+        else:
+            stack.delete(stack_name)
+            module.exit_json(changed = True, result = "deleted")
+    elif module.params['state'] == 'list':
+        stack_list = stack.list()
+        module.exit_json(changed = True, result = "list" , stack_list = stack_list)
+    else:
+            module.exit_json(changed = False, result = "Not implemented yet")
 
-    except shade.OpenStackCloudException as e:
-        module.fail_json(msg=e.message)
-
-# this is magic, see lib/ansible/module_common.py
 from ansible.module_utils.basic import *
 from ansible.module_utils.openstack import *
 if __name__ == '__main__':
